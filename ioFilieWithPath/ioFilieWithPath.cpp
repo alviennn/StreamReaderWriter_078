34adc10696d8878f382ba1651411aa75fa14cb13
alviennn <alvien.rnp@gmail.com>
2023-06-21 11:26:41 +0700

    manual exception untuk mengeksekusi jika terjadi error, terdapat 3 unsur yaitu try, throw dan catch

--- ioFilieWithPath/ioFilieWithPath.cpp
+++ ioFilieWithPath/ioFilieWithPath.cpp
@@ -49,6 +49,6 @@ int main(){
 		infile.close();
 	}
 	//jika tidak ditemukan file maka akan menampilkan ini
-	else cout << "unable to open file";
+	else cout << "Unable to open file";
 	return 0;
 }
